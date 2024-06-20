303ce5ce595247a8791f203476ee68e9afe1d8b7
Hernan Diaz <hernan@pollen.com.au>
2024-06-20 21:39:38 +1000

    Install react and tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,15 @@
-import { defineConfig } from 'astro/config';
-
+import { defineConfig } from "astro/config";
 import cloudflare from "@astrojs/cloudflare";
+import tailwind from "@astrojs/tailwind";
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   adapter: cloudflare({
     platformProxy: {
-      enabled: true
-    }
-  })
+      enabled: true,
+    },
+  }),
+  integrations: [tailwind(), react()],
 });
